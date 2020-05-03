48d23f4b472ce8fca4d646a9b68fc461e97936cd
Ricardo Meireles <cadomeireles@gmail.com>
2020-05-03 11:09:22 -0300

    Create dashboard page
    - app URL included on root URL conf
    - view and template created
    - set the view as new LOGIN_REDIRECT_URL

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,23 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client, TestCase
 
-# Create your tests here.
+
+class DashboardTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_redirect_to_dashboard_after_login(self):
+        user = User.objects.create_user(
+            email='tester@email.com', username='tester')
+        user.set_password('Tes#es12_')
+        user.save()
+
+        post_data = {
+            'login': 'tester@email.com', 'password': 'Tes#es12_'
+        }
+
+        response = self.client.post('/accounts/login/', post_data)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/dashboard')
